841be0aa0c32099ad06c4d7f365a257c5ea03e51
Gesa Neubauer <102871739+gesaneubauer@users.noreply.github.com>
2023-11-17 16:28:37 +0100

    Created using Colaboratory

---
+++ main.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOmWdw+5sYUReHyoU8EqPxb",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/gesaneubauer/AdvancesDataMining/blob/main/main.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "SVW7r_MB5Q-5"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
